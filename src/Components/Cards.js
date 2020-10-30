2265c115c247b3fa7084422d47b4a598d8743457
Junior <juniormbaya243@gmail.com>
2020-10-30 21:35:34 +0100

    fix issue with slack side

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Box,
   Card,
@@ -9,7 +9,10 @@ import {
   createMuiTheme,
   makeStyles,
   Container,
+  ThemeProvider,
+  Button,
 } from "@material-ui/core";
+import gsap from "gsap";
 import Test from "../assets/paris.jpeg";
 
 function Cards() {
@@ -19,10 +22,10 @@ function Cards() {
       display: "flex",
     },
     cardSlide: {
-      left: 10,
-      zIndex: -1,
-      border: "1px solid red",
+      left: 20,
+      marginLeft: "5px",
       position: "absolute",
+      zIndex: -1,
     },
     contain: {
       display: "flex",
@@ -31,42 +34,176 @@ function Cards() {
   });
   const classes = useStyles();
 
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#000",
+      },
+      secondary: {
+        main: "#fed330",
+      },
+    },
+  });
+
+  const [stack, setStack] = useState({
+    initial: false,
+    clicked: null,
+    nameClick: "View the Stack",
+  });
+
+  //handle
+  const handleClick = () => {
+    if (stack.initial === false) {
+      setStack({
+        initial: null,
+        clicked: true,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 1");
+    } else if (stack.clicked === true) {
+      setStack({
+        clicked: !stack.clicked,
+        nameClick: "View the Stack",
+      });
+      console.log("Click 2");
+    } else if (stack.clicked === false) {
+      setStack({
+        clicked: !stack.clicked,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 3");
+    }
+  };
+  //useRef
+  let slideRef = useRef(null);
+
+  //use my useEffect below
+
+  useEffect(() => {
+    if (stack.clicked === false) {
+      gsap.to(slideRef, { duration: 1, x: 0, backgroundColor: "blue" });
+    } else if (
+      stack.clicked === true ||
+      (stack.clicked === true && stack.initial === null)
+    ) {
+      gsap.to(slideRef, { duration: 1, x: 250, backgroundColor: "orange" });
+    }
+  });
+
   return (
-    <div style={{ border: "1px solid green", with: "800px" }}>
+    <div style={{ with: "800px" }}>
       <Container className={classes.contain} maxWidth="lg">
-        <Box className={classes.root}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image={Test}
-                width="100%"
-                height="200"
-              />
-            </CardActionArea>
-            <CardContent>
-              <Typography>LeBoncoin Réplica</Typography>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box className={classes.cardSlide}>
-          <Card>
-            <CardContent>
-              <Typography>
-                <Box> FrontEnd</Box>
-                <Box> React</Box>
-                <Box> HTML & CSS</Box>
-                <Box> Call API </Box>
-                <br />
-                <br />
-                <Box>BackEnd</Box>
-                <Box> Nodejs </Box>
-                <Box> Mongodb</Box>
-                <Box> Express</Box>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Box>
+        <ThemeProvider theme={theme}>
+          <Box className={classes.root}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={Test}
+                  width="100%"
+                  height="200"
+                />
+              </CardActionArea>
+              <CardContent>
+                <Typography>LeBoncoin Réplica</Typography>
+                <Box
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Button
+                    onClick={handleClick}
+                    size="small"
+                    color="secondary"
+                    variant="contained"
+                  >
+                    {stack.nameClick}
+                  </Button>
+                  <Button
+                    style={{ marginLeft: "15px" }}
+                    size="small"
+                    color="secondary"
+                    variant="outlined"
+                  >
+                    View the Code
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box className={classes.cardSlide}>
+            <Card ref={(el) => (slideRef = el)}>
+              <CardContent>
+                <Typography>
+                  <Box> FrontEnd</Box>
+                  <Box> React</Box>
+                  <Box> HTML & CSS</Box>
+                  <Box> Firebase </Box>
+                  <br />
+                  <Box>BackEnd</Box>
+                  <Box> Nodejs </Box>
+                  <Box> Mongodb</Box>
+                  <Box> Express</Box>
+                  <Box> Stripe</Box>
+                  <Box> Heroku</Box>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          {/* second */}
+          <Box className={classes.root}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={Test}
+                  width="100%"
+                  height="200"
+                />
+              </CardActionArea>
+              <CardContent>
+                <Typography>Covid 19</Typography>
+                <Box
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Button
+                    onClick={handleClick}
+                    size="small"
+                    color="secondary"
+                    variant="contained"
+                  >
+                    {stack.nameClick}
+                  </Button>
+                  <Button
+                    style={{ marginLeft: "15px" }}
+                    size="small"
+                    color="secondary"
+                    variant="outlined"
+                  >
+                    View the Code
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box className={classes.cardSlide}>
+            <Card ref={(el) => (slideRef = el)}>
+              <CardContent>
+                <Typography>
+                  <Box> FrontEnd</Box>
+                  <Box> React</Box>
+                  <Box> HTML & CSS</Box>
+                  <Box> Firebase </Box>
+                  <br />
+                  <Box>BackEnd</Box>
+                  <Box> Nodejs </Box>
+                  <Box> Mongodb</Box>
+                  <Box> Express</Box>
+                  <Box> Stripe</Box>
+                  <Box> Heroku</Box>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </ThemeProvider>
       </Container>
     </div>
   );
