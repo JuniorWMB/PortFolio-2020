b93b5589579322d9ffa278ff2c9bbfdc67c97d56
Junior <juniormbaya243@gmail.com>
2020-11-01 23:59:50 +0100

    some

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -27,6 +27,20 @@ function Cards() {
       marginLeft: "5px",
       position: "absolute",
       zIndex: -1,
+      border: "4px solid blue",
+    },
+    cardSlide2: {
+      left: 420,
+      marginLeft: "5px",
+      position: "absolute",
+      zIndex: -1,
+    },
+    cardSlide3: {
+      left: 810,
+      marginLeft: "5px",
+      position: "absolute",
+      border: "4px solid blue",
+      zIndex: -1,
     },
     contain: {
       display: "flex",
@@ -50,7 +64,18 @@ function Cards() {
     },
   });
 
+  // state
   const [stack, setStack] = useState({
+    initial: false,
+    clicked: null,
+    nameClick: "View the Stack",
+  });
+  const [stack2, setStack2] = useState({
+    initial: false,
+    clicked: null,
+    nameClick: "View the Stack2",
+  });
+  const [stack3, setStack3] = useState({
     initial: false,
     clicked: null,
     nameClick: "View the Stack",
@@ -64,6 +89,7 @@ function Cards() {
         clicked: true,
         nameClick: "Close the Stack",
       });
+
       console.log("Click 1");
     } else if (stack.clicked === true) {
       setStack({
@@ -79,6 +105,52 @@ function Cards() {
       console.log("Click 3");
     }
   };
+  const handleClick2 = () => {
+    if (stack2.initial === false) {
+      setStack2({
+        initial: null,
+        clicked: true,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 1");
+    } else if (stack2.clicked === true) {
+      setStack2({
+        clicked: !stack2.clicked,
+        nameClick: "View the Stack",
+      });
+      console.log("Click 2");
+    } else if (stack2.clicked === false) {
+      setStack2({
+        clicked: !stack2.clicked,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 3");
+    }
+  };
+
+  const handleClick3 = () => {
+    if (stack3.initial === false) {
+      setStack3({
+        initial: null,
+        clicked: true,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 1");
+    } else if (stack3.clicked === true) {
+      setStack3({
+        clicked: !stack3.clicked,
+        nameClick: "View the Stack",
+      });
+      console.log("Click 2");
+    } else if (stack3.clicked === false) {
+      setStack3({
+        clicked: !stack3.clicked,
+        nameClick: "Close the Stack",
+      });
+      console.log("Click 3");
+    }
+  };
+
   //useRef
   let slideRef = useRef(null);
 
@@ -90,6 +162,14 @@ function Cards() {
     } else if (
       stack.clicked === true ||
       (stack.clicked === true && stack.initial === null)
+    ) {
+      gsap.to(slideRef, { duration: 1, x: 250, backgroundColor: "orange" });
+    }
+    if (stack2.clicked === false) {
+      gsap.to(slideRef, { duration: 1, x: 0, backgroundColor: "blue" });
+    } else if (
+      stack2.clicked === true ||
+      (stack2.clicked === true && stack2.initial === null)
     ) {
       gsap.to(slideRef, { duration: 1, x: 250, backgroundColor: "orange" });
     }
@@ -171,12 +251,12 @@ function Cards() {
               <Typography>Covid-19 Tracker</Typography>
               <Box style={{ display: "flex", justifyContent: "space-between" }}>
                 <Button
-                  onClick={handleClick}
+                  onClick={handleClick2}
                   size="small"
                   color="secondary"
                   variant="contained"
                 >
-                  {stack.nameClick}
+                  {stack2.nameClick}
                 </Button>
                 <Button
                   style={{ marginLeft: "15px" }}
@@ -189,6 +269,25 @@ function Cards() {
               </Box>
             </CardContent>
           </Card>
+          <Box className={classes.cardSlide2}>
+            <Card ref={(el) => (slideRef = el)}>
+              <CardContent>
+                <Typography>
+                  <Box> FrontEnd</Box>
+                  <Box> React</Box>
+                  <Box> HTML & CSS</Box>
+                  <Box> Firebase </Box>
+                  <br />
+                  <Box>BackEnd</Box>
+                  <Box> Nodejs </Box>
+                  <Box> Mongodb</Box>
+                  <Box> Express</Box>
+                  <Box> Stripe</Box>
+                  <Box> Heroku</Box>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
         </Box>
         {/* second Block */}
         <Box className={classes.root}>
@@ -205,12 +304,12 @@ function Cards() {
               <Typography>Marvel Comics</Typography>
               <Box style={{ display: "flex", justifyContent: "space-between" }}>
                 <Button
-                  onClick={handleClick}
+                  onClick={handleClick3}
                   size="small"
                   color="secondary"
                   variant="contained"
                 >
-                  {stack.nameClick}
+                  {stack3.nameClick}
                 </Button>
                 <Button
                   style={{ marginLeft: "15px" }}
@@ -223,6 +322,25 @@ function Cards() {
               </Box>
             </CardContent>
           </Card>
+          <Box className={classes.cardSlide3}>
+            <Card ref={(el) => (slideRef = el)}>
+              <CardContent>
+                <Typography>
+                  <Box> FrontEnd</Box>
+                  <Box> React</Box>
+                  <Box> HTML & CSS</Box>
+                  <Box> Firebase </Box>
+                  <br />
+                  <Box>BackEnd</Box>
+                  <Box> Nodejs </Box>
+                  <Box> Mongodb</Box>
+                  <Box> Express</Box>
+                  <Box> Stripe</Box>
+                  <Box> Heroku</Box>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
         </Box>
       </Container>
     </div>
