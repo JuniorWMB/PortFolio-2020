78919d4433af96d0ec520f18f3806fecefb88b11
Junior <juniormbaya243@gmail.com>
2020-11-09 17:15:39 +0000

    add contact post

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { ArrowDownCircle, Check, ArrowRightCircle } from "react-feather";
 import gsap from "gsap";
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
+import axios from "axios";
 import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
 
@@ -40,11 +39,41 @@ function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
+  const [errorText, setErrorText] = useState("");
 
-  // const handleSend = () => {
-  //   gsap.from(arrowSendRef, { duration: 1, ease: "powe3.inOut", width: 0 });
+  //creat my handle
+  const handleClick = () => {
+    setErrorText(!errorText);
+  };
 
-  // };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMobileChange = (e) => {
+    setMobile(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    // e.target.reset();
+    e.preventDefault();
+
+    if (name && email && mobile) {
+      await axios.post(
+        "https://portfolio-fomulaire.herokuapp.com/portfolio-contact",
+        {
+          email,
+          name,
+          mobile,
+        }
+      );
+    } else {
+      setErrorText("Alfred something missing !");
+    }
+  };
+  console.log("name", handleSubmit);
 
   return (
     <div className="contact">
@@ -57,10 +86,16 @@ function Contact() {
           <ArrowDownCircle ref={(el) => (arrowRef = el)} size={50} />
         </div>
       </div>
-      <form className="contact__form">
+      <form
+        onSubmit={handleSubmit}
+        className="contact__form"
+        autoComplete="off"
+      >
         <p className="contact__forminputpara">
           Hello, my name is
           <input
+            onChange={handleNameChange}
+            value={name}
             className="form__input"
             type="text"
             size="40px"
@@ -68,22 +103,53 @@ function Contact() {
           />
           and i'd like to work with you. <br /> My email address is
           <input
+            onChange={handleEmailChange}
+            value={email}
             className="form__inputthree"
             type="email"
             placeholder="Batman@gothamcity.io"
-          />{" "}
+            autoComplete="false"
+          />
           and my phone number is
           <input
+            onChange={handleMobileChange}
+            value={mobile}
             className="form__inputtwo"
-            type="number"
+            type="tel"
             placeholder="0620201102"
           />
           .
           <br />
         </p>
+        {errorText && (
+          <p className="error">
+            {errorText}
+            <button
+              style={{
+                width: "300px",
+                height: "50px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "20px",
+                backgroundColor: "red",
+                border: "none",
+                outline: "none",
+                cursor: "pointer",
+                color: "white",
+              }}
+              type="submit"
+              onClick={handleClick}
+            >
+              <h1> Try again !</h1>
+            </button>
+          </p>
+        )}
 
+        {/* 
         <ThemeProvider theme={theme}>
           <Button
+          type=""
             className={classes.button}
             variant="contained"
             color="secondary"
@@ -99,7 +165,27 @@ function Contact() {
           >
             Send
           </Button>
-        </ThemeProvider>
+        </ThemeProvider> */}
+        {!errorText && (
+          <button
+            style={{
+              width: "300px",
+              height: "50px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "20px",
+              backgroundColor: "#fed330",
+              border: "none",
+              outline: "none",
+              cursor: "pointer",
+              color: "white",
+            }}
+            type="submit"
+          >
+            <h1> Send</h1>
+          </button>
+        )}
       </form>
     </div>
   );
