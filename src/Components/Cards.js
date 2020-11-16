1a0c13d75bb9a26123effd54461cc5172d8e8bc8
Junior <juniormbaya243@gmail.com>
2020-11-16 15:17:59 +0000

    ok

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,180 +1,180 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
-  createMuiTheme,
-  makeStyles,
-  Button,
-} from "@material-ui/core";
+import React from "react";
+// import {
+//   Box,
+//   Card,
+//   CardActionArea,
+//   CardContent,
+//   CardMedia,
+//   Typography,
+//   createMuiTheme,
+//   makeStyles,
+//   Button,
+// } from "@material-ui/core";
 import "./card.css";
-import gsap from "gsap";
+// import gsap from "gsap";
 import Lbc from "../assets/lbc.jpg";
 import Covid from "../assets/covid.jpg";
 import Marvel from "../assets/marvelbon.png";
 import Trip from "../assets/trip.jpg";
 
 function Cards() {
-  const useStyles = makeStyles({
-    root: {
-      maxWidth: 245,
-      display: "flex",
-      marginRight: "150px",
-    },
-    cardSlide: {
-      left: 24,
-      marginLeft: "2px",
-      position: "absolute",
-      zIndex: -1,
-    },
-    cardSlide2: {
-      left: 420,
-      marginLeft: "2px",
-      position: "absolute",
-      zIndex: -1,
-    },
-    cardSlide3: {
-      left: 810,
-      marginLeft: "5px",
-      position: "absolute",
-      zIndex: -1,
-    },
-    contain: {
-      display: "flex",
-      position: "relative",
-      flexDirection: "row",
-      flexWrap: "wrap",
-      width: "1280px",
-    },
-  });
-  const classes = useStyles();
+  // const useStyles = makeStyles({
+  //   root: {
+  //     maxWidth: 245,
+  //     display: "flex",
+  //     marginRight: "150px",
+  //   },
+  //   cardSlide: {
+  //     left: 24,
+  //     marginLeft: "2px",
+  //     position: "absolute",
+  //     zIndex: -1,
+  //   },
+  //   cardSlide2: {
+  //     left: 420,
+  //     marginLeft: "2px",
+  //     position: "absolute",
+  //     zIndex: -1,
+  //   },
+  //   cardSlide3: {
+  //     left: 810,
+  //     marginLeft: "5px",
+  //     position: "absolute",
+  //     zIndex: -1,
+  //   },
+  //   contain: {
+  //     display: "flex",
+  //     position: "relative",
+  //     flexDirection: "row",
+  //     flexWrap: "wrap",
+  //     width: "1280px",
+  //   },
+  // });
+  // const classes = useStyles();
 
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: "#000",
-      },
-      secondary: {
-        main: "#fed330",
-      },
-    },
-  });
+  // const theme = createMuiTheme({
+  //   palette: {
+  //     primary: {
+  //       main: "#000",
+  //     },
+  //     secondary: {
+  //       main: "#fed330",
+  //     },
+  //   },
+  // });
 
-  // state
-  const [stack, setStack] = useState({
-    initial: false,
-    clicked: null,
-    nameClick: "View the Stack",
-  });
-  const [stack2, setStack2] = useState({
-    initial: false,
-    clicked: null,
-    nameClick: "View the Stack",
-  });
-  const [stack3, setStack3] = useState({
-    initial: false,
-    clicked: null,
-    nameClick: "View the Stack",
-  });
+  // // state
+  // const [stack, setStack] = useState({
+  //   initial: false,
+  //   clicked: null,
+  //   nameClick: "View the Stack",
+  // });
+  // const [stack2, setStack2] = useState({
+  //   initial: false,
+  //   clicked: null,
+  //   nameClick: "View the Stack",
+  // });
+  // const [stack3, setStack3] = useState({
+  //   initial: false,
+  //   clicked: null,
+  //   nameClick: "View the Stack",
+  // });
 
-  //handle
-  const handleClick = () => {
-    if (stack.initial === false) {
-      setStack({
-        initial: null,
-        clicked: true,
-        nameClick: "Close the Stack",
-      });
-    } else if (stack.clicked === true) {
-      setStack({
-        clicked: !stack.clicked,
-        nameClick: "View the Stack",
-      });
-    } else if (stack.clicked === false) {
-      setStack({
-        clicked: !stack.clicked,
-        nameClick: "Close the Stack",
-      });
-    }
-  };
-  const handleClick2 = () => {
-    if (stack2.initial === false) {
-      setStack2({
-        initial: null,
-        clicked: true,
-        nameClick: "Close the Stack",
-      });
-    } else if (stack2.clicked === true) {
-      setStack2({
-        clicked: !stack2.clicked,
-        nameClick: "View the Stack",
-      });
-    } else if (stack2.clicked === false) {
-      setStack2({
-        clicked: !stack2.clicked,
-        nameClick: "Close the Stack",
-      });
-    }
-  };
+  // //handle
+  // const handleClick = () => {
+  //   if (stack.initial === false) {
+  //     setStack({
+  //       initial: null,
+  //       clicked: true,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   } else if (stack.clicked === true) {
+  //     setStack({
+  //       clicked: !stack.clicked,
+  //       nameClick: "View the Stack",
+  //     });
+  //   } else if (stack.clicked === false) {
+  //     setStack({
+  //       clicked: !stack.clicked,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   }
+  // };
+  // const handleClick2 = () => {
+  //   if (stack2.initial === false) {
+  //     setStack2({
+  //       initial: null,
+  //       clicked: true,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   } else if (stack2.clicked === true) {
+  //     setStack2({
+  //       clicked: !stack2.clicked,
+  //       nameClick: "View the Stack",
+  //     });
+  //   } else if (stack2.clicked === false) {
+  //     setStack2({
+  //       clicked: !stack2.clicked,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   }
+  // };
 
-  const handleClick3 = () => {
-    if (stack3.initial === false) {
-      setStack3({
-        initial: null,
-        clicked: true,
-        nameClick: "Close the Stack",
-      });
-    } else if (stack3.clicked === true) {
-      setStack3({
-        clicked: !stack3.clicked,
-        nameClick: "View the Stack",
-      });
-    } else if (stack3.clicked === false) {
-      setStack3({
-        clicked: !stack3.clicked,
-        nameClick: "Close the Stack",
-      });
-    }
-  };
+  // const handleClick3 = () => {
+  //   if (stack3.initial === false) {
+  //     setStack3({
+  //       initial: null,
+  //       clicked: true,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   } else if (stack3.clicked === true) {
+  //     setStack3({
+  //       clicked: !stack3.clicked,
+  //       nameClick: "View the Stack",
+  //     });
+  //   } else if (stack3.clicked === false) {
+  //     setStack3({
+  //       clicked: !stack3.clicked,
+  //       nameClick: "Close the Stack",
+  //     });
+  //   }
+  // };
 
   //useRef
-  let slideRef = useRef(null);
-  let slideRef2 = useRef(null);
-  let slideRef3 = useRef(null);
+  // let slideRef = useRef(null);
+  // let slideRef2 = useRef(null);
+  // let slideRef3 = useRef(null);
 
   //use my useEffect below
 
-  useEffect(() => {
-    if (stack.clicked === false) {
-      gsap.to(slideRef, { duration: 1, x: 0, backgroundColor: "blue" });
-    } else if (
-      stack.clicked === true ||
-      (stack.clicked === true && stack.initial === null)
-    ) {
-      gsap.to(slideRef, { duration: 1, x: 230, backgroundColor: "orange" });
-    }
+  // useEffect(() => {
+  //   if (stack.clicked === false) {
+  //     gsap.to(slideRef, { duration: 1, x: 0, backgroundColor: "blue" });
+  //   } else if (
+  //     stack.clicked === true ||
+  //     (stack.clicked === true && stack.initial === null)
+  //   ) {
+  //     gsap.to(slideRef, { duration: 1, x: 230, backgroundColor: "orange" });
+  //   }
 
-    if (stack2.clicked === false) {
-      gsap.to(slideRef2, { duration: 1, x: 0, backgroundColor: "blue" });
-    } else if (
-      stack2.clicked === true ||
-      (stack2.clicked === true && stack2.initial === null)
-    ) {
-      gsap.to(slideRef2, { duration: 1, x: 230, backgroundColor: "orange" });
-    }
+  //   if (stack2.clicked === false) {
+  //     gsap.to(slideRef2, { duration: 1, x: 0, backgroundColor: "blue" });
+  //   } else if (
+  //     stack2.clicked === true ||
+  //     (stack2.clicked === true && stack2.initial === null)
+  //   ) {
+  //     gsap.to(slideRef2, { duration: 1, x: 230, backgroundColor: "orange" });
+  //   }
 
-    if (stack3.clicked === false) {
-      gsap.to(slideRef3, { duration: 1, x: 0, backgroundColor: "blue" });
-    } else if (
-      stack3.clicked === true ||
-      (stack3.clicked === true && stack3.initial === null)
-    ) {
-      gsap.to(slideRef3, { duration: 1, x: 250, backgroundColor: "orange" });
-    }
-  });
+  //   if (stack3.clicked === false) {
+  //     gsap.to(slideRef3, { duration: 1, x: 0, backgroundColor: "blue" });
+  //   } else if (
+  //     stack3.clicked === true ||
+  //     (stack3.clicked === true && stack3.initial === null)
+  //   ) {
+  //     gsap.to(slideRef3, { duration: 1, x: 250, backgroundColor: "orange" });
+  //   }
+  // });
 
   return (
     <div className="cards">
